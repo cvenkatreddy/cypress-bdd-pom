37bcb7e593af1f636ebddb51fbd0e2d9271da17f
cvenkatreddy <venkatreddy.chinta@gmail.com>
2022-01-25 11:03:00 +0100

    Chained verifyGameLaunch

--- cypress/pageobjects/GamePage.ts
+++ cypress/pageobjects/GamePage.ts
@@ -4,10 +4,11 @@ const gamePageHeader = "h1[scale='1']";
 
 export const GamePage = {
   verifyGameLaunch(gamePageUrl: string) {
-    VerifyPageUrl(gamePageUrl);
-    cy.get(gamePageHeader).should(
-      "have.text",
-      "Book of Dead Slot at Ninja Casino!"
-    );
+    VerifyPageUrl(gamePageUrl).then(() => {
+      cy.get(gamePageHeader).should(
+        "have.text",
+        "Book of Dead Slot at Ninja Casino!"
+      );
+    });
   },
 };
